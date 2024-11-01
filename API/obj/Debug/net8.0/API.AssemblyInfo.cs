e3588b7ba7a8547133092d4ad40b5ac58bf08219
NihadMandzo <nihad.mandzo@edu.fit.ba>
2024-11-01 18:33:14 +0100

    Section 6

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3492e198333ec55383902a67c467186bc5c60615")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3185e028d400fa9da3f13d2917760f077e83fa1")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -14,6 +14,7 @@ import { HomeComponent } from "./home/home.component";
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit{
+  [x: string]: any;
   private accountService=inject(AccountService);
   
   ngOnInit(): void {
